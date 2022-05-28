d58c66a6e5afd6dffb9ef2d5c9e77c91fd89b2c5
Kurt Baux <ktripodibaux@gmail.com>
2022-05-28 12:13:29 -0400

    didnt comment enough

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+
+ const main = document.querySelector('.main')
+const lower = document.querySelector('.lower');
+const jav = document.querySelector('.jav');
+const button = document.querySelector('.button');
+
+ const click = jav.addEventListener('click', function(){
+    javer();
+ });
+
+const back = button.addEventListener('click', function() {
+    javer2();
+})
+
+const javer = function() {
+    lower.classList.remove('hidden');
+    lower.classList.remove('hidden');
+    main.classList.add('hidden');
+ };
+ const javer2 = function() {
+    main.classList.remove('hidden');
+    lower.classList.add('hidden');
+ }
